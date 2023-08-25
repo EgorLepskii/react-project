efb3276ccc07be28daae5eeb7c94f333768ee6b5
Egor <egor.lepskii@mail.ru>
2023-08-25 22:15:58 +0700

    like component completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,19 @@
 "use client";
 import styles from './page.module.css';
-import Button from "@/app/components/Button/Button";
-import P from "@/app/components/P/P";
-import Tag from "@/app/components/Tag/Tag";
-import Card from "@/app/components/Card/Card";
-import Rating from "@/app/components/Rating/Rating";
-import {useState} from "react";
+import Like from "@/app/components/Like/Like";
+import axios from "axios";
 
 export default function Home() {
-    const [rating, setRating] = useState(4);
-  return (
-    <main className={styles.main}>
-        <Rating rating={rating} style={{width:"200px"}} isEditable setRating={setRating}/>
-          <Card header={'Как работать с CSS Grid'}
-                description={<><a href={'https://google.com'}>Грид-раскладка (CSS Grid Layout)</a> представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..</>}
-                likes={4}
-                time={'3 минуты'}
-                timeToRead={'1 месяц назад'}
-                tag={'Front-End'}
-                imageLink={'/mini.png'}
 
-          />
-        <Button appear={'ghost'} direction={'right'}>Читать отзывы</Button>
-        <Button appear={'primary'} direction={'right'}>Читать отзывы</Button>
-        <Button appear={'ghost'} direction={'down'}>Читать отзывы</Button>
-        <Button appear={'primary'} direction={'down'}>Читать отзывы</Button>
-        <P size={'m'}>test</P>
-        <P size={'l'}>test</P>
-        <P size={'s'}>test</P>
-        <Tag appear={'green'} size={'s'}>10000</Tag>
-        <Tag appear={'ghost'} size={'s'}>10</Tag>
-        <Tag appear={'red'} size={'s'}>hh.ru</Tag>
-        <Tag appear={'primary'} size={'s'}>Подготовка макетов</Tag>
-        <Tag appear={'green'} size={'m'}>10000</Tag>
-        <Tag appear={'ghost'} size={'m'}>10</Tag>
-        <Tag appear={'red'} size={'m'}>hh.ru</Tag>
-        <Tag appear={'primary'} size={'m'}>Подготовка макетов</Tag>
-        <Tag appear={'primary'} size={'m'} href={'https://google.com'}>Подготовка макетов</Tag>
-    </main>
-  );
+    const sendRequest = () => {
+        axios.patch('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
+            console.log(response.data);
+        }).catch((e) => console.log(e));
+    };
+
+    return (
+        <main className={styles.main}>
+            <Like handleLikeChange={() => sendRequest()}/>
+        </main>
+    );
 }

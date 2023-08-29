ac2c86939308e4bd21e7b0a816f4b04d22dba931
Egor <egor.lepskii@mail.ru>
2023-08-29 17:44:21 +0700

    layout completed

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import Card from "@/components/Card/Card";
+import styles from './Page.module.css';
+import cn from "classnames";
 
 const Page = () => {
     return (
-        <div>
-            blog
+        <div className={cn(styles.page)}>
+            <Card header='Как работать с CSS Grid'
+                  description={<><a href='https://google.com'>Грид-раскладка (CSS Grid Layout)</a> представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..</>}
+                  likes={4}
+                  time='3 минуты'
+                  timeToRead='1 месяц назад'
+                  tag='Front-End'
+                  imageLink='/mini.png'
+
+            />
+            <Card header='Как работать с CSS Grid'
+                  description={<><a href='https://google.com'>Грид-раскладка (CSS Grid Layout)</a> представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..</>}
+                  likes={4}
+                  time='3 минуты'
+                  timeToRead='1 месяц назад'
+                  tag='Front-End'
+                  imageLink='/mini.png'
+
+            />
+            <Card header='Как работать с CSS Grid'
+                  description={<><a href='https://google.com'>Грид-раскладка (CSS Grid Layout)</a> представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..</>}
+                  likes={4}
+                  time='3 минуты'
+                  timeToRead='1 месяц назад'
+                  tag='Front-End'
+                  imageLink='/mini.png'
+
+            />
         </div>
     );
 };

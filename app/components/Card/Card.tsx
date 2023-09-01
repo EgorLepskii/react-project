30ed2f5a1e7eb22ecd8498ebf2dd41a68f78dcd3
Egor <egor.lepskii@mail.ru>
2023-09-01 10:03:31 +0700

    next/image component added

--- app/components/Card/Card.tsx
+++ app/components/Card/Card.tsx
@@ -4,12 +4,17 @@ import cn from "classnames";
 import Likes from "@/app/components/Likes/Likes";
 import {CardProps} from "@/app/components/Card/Card.props";
 import Read from "@/app/components/Read/Read";
+import Image from "next/image";
 
 const Card = ({header, description, timeToRead, time, likes, tag, imageLink}: CardProps) => {
     return (
         <div className={cn(styles.card)}>
             <div className={cn(styles.media)}>
-                <img src={imageLink} alt={'media'}/>
+                <Image
+                    src={imageLink}
+                    alt={'media'}
+                    fill={true}
+                />
             </div>
             <div className={cn(styles.info)}>
                 <div className={cn(styles.info__tag)}>

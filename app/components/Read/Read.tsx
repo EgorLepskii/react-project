bfbbd294db63f9c9ec784359960125f5c1338835
Egor <egor.lepskii@mail.ru>
2023-09-01 10:44:12 +0700

    added next/link

--- app/components/Read/Read.tsx
+++ app/components/Read/Read.tsx
@@ -2,15 +2,16 @@ import React from 'react';
 import {ReadProps} from "@/app/components/Read/Read.props";
 import ReadArrow from '../../../public/arrow.svg';
 import styles from './Read.module.css';
+import Link from "next/link";
 
 const Read = ({text, link}: ReadProps) => {
     return (
-        <a href={link}>
+        <Link href={link}>
             <div className={styles.read}>
                 <p>{text}</p>
                 <ReadArrow/>
             </div>
-        </a>
+        </Link>
     );
 };
 

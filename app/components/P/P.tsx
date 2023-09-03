4c3785507910a59a1932d038abbc5cc57df028e9
Egor <egor.lepskii@mail.ru>
2023-09-03 17:39:47 +0700

    classes moved to variables

--- app/components/P/P.tsx
+++ app/components/P/P.tsx
@@ -3,14 +3,16 @@ import cn from "classnames";
 import styles from './P.module.css';
 
 const P = ({size = 'm', children, className, ...props}: PProps) => {
+    const pClasses = cn(styles.p, className,
+        {
+            [styles.small]: size === 's',
+            [styles.medium]: size === 'm',
+            [styles.large]: size === 'l'
+        });
+
     return (
-        <p {...props} className={cn(styles.p, className,
-            {
-                [styles.small]: size === 's',
-                [styles.medium]: size === 'm',
-                [styles.large]: size === 'l'
-            })
-        }
+        <p {...props}
+           className={pClasses}
         >
             {children}
         </p>
--- app/components/Tag/Tag.tsx
+++ app/components/Tag/Tag.tsx
@@ -3,20 +3,22 @@ import styles from './Tag.module.css';
 import cn from "classnames";
 
 const Tag = ({appear = 'ghost', size, children, className, href, ...props}: TagProps) => {
+    const classes = cn(styles.tag, className,
+        {
+            [styles.ghost]: appear === 'ghost',
+            [styles.red]: appear === 'red',
+            [styles.primary]: appear === 'primary',
+            [styles.green]: appear === 'green',
+            [styles.m]: size === 'm',
+            [styles.s]: size === 's',
+        });
+
     return (
 
         <a
             {...props}
             href={href}
-            className={cn(styles.tag, className,
-                {
-                    [styles.ghost]: appear === 'ghost',
-                    [styles.red]: appear === 'red',
-                    [styles.primary]: appear === 'primary',
-                    [styles.green]: appear === 'green',
-                    [styles.m]: size === 'm',
-                    [styles.s]: size === 's',
-                })}
+            className={classes}
         >
             {children}
         </a>

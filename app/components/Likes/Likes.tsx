afc3e16108e2b0edeafdaba673402965b23d6e37
Egor <egor.lepskii@mail.ru>
2023-09-03 17:16:28 +0700

    likes updated to use button

--- app/components/Likes/Likes.tsx
+++ app/components/Likes/Likes.tsx
@@ -5,11 +5,9 @@ import Like from '@/public/icons/like.svg';
 
 const Likes = ({count}: LikesProps) => {
     return (
-        <button className={cn(styles.btn)}>
-            <div className={cn(styles.likes)}>
-                <span>{count}</span>
-                <Like/>
-            </div>
+        <button className={cn(styles.likes)}>
+            <span>{count}</span>
+            <Like/>
         </button>
     );
 };

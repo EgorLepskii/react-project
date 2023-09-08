4dac5eb08ca22b0dc77dc283a223d0554263eda5
Egor <egor.lepskii@mail.ru>
2023-09-08 20:09:34 +0700

    added next/link to footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,12 +1,13 @@
 import {FooterProps} from "@/components/Footer/Footer.props";
 import styles from './Footer.module.css';
+import Link from "next/link";
 
 const Footer = ({}: FooterProps) => {
     return (
         <div className={styles.footer}>
             <span className='e1'>OwlTop © 2020 - 2021 Все права защищены</span>
-            <a href='#' className='e2'>Пользовательское соглашение</a>
-            <a href='#' className='e3'>Политика конфиденциальности</a>
+            <Link href='#' className='e2'>Пользовательское соглашение</Link>
+            <Link href='#' className='e3'>Политика конфиденциальности</Link>
         </div>
     );
 };

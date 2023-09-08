ec9f54f743d31a7ae48fa3adafa2428870c3a505
Egor <egor.lepskii@mail.ru>
2023-09-08 19:50:58 +0700

    send moved to Like component

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
-"use client";
 import styles from './page.module.css';
 import Like from "@/components/Like/Like";
-import axios from "axios";
 
 export default function Home() {
-    const sendRequest = () => {
-        axios.patch('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
-            console.log(response.data);
-        }).catch((e) => console.log(e));
-    };
+
 
     return (
         <main className={styles.main}>
-            <Like handleLikeChange={() => sendRequest()}/>
+            <Like/>
         </main>
     );
 }
--- components/Like/Like.tsx
+++ components/Like/Like.tsx
@@ -1,18 +1,23 @@
+"use client";
 import {useState} from 'react';
 import LikeIcon from '../../public/shape.svg';
 import styles from './Like.module.css';
-import {LikeProps} from "@/components/Like/Like.props";
 import cn from "classnames";
+import axios from "axios";
 
-const Like = ({handleLikeChange}: LikeProps) => {
+const Like = () => {
     const [active, setActive] = useState<boolean>(false);
+    const sendRequest = () => {
+        axios.patch('https://jsonplaceholder.typicode.com/posts/1').then((response) => {
+            console.log(response.data);
+        }).catch((e) => console.log(e));
+    };
 
     return (
         <div className={cn(styles.like, {[styles.active]: active, [styles.notActive]: !active})}
              onClick={() => {
                  setActive(!active);
-                 if (handleLikeChange)
-                     handleLikeChange();
+                 sendRequest();
              }}>
             <LikeIcon/>
         </div>

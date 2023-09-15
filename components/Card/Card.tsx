9e485fa35c426a8f4b094dd7b5f95a095ea13d75
Egor <egor.lepskii@mail.ru>
2023-09-15 19:39:24 +0700

    added fill for image

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -5,16 +5,30 @@ import {CardProps} from "@/components/Card/Card.props";
 import Read from "@/components/Read/Read";
 import Image from "next/image";
 
-const Card = ({header, description, timeToRead, time, likes, tag, imageLink, postId, className, ...props}: CardProps) => {
+const Card = ({
+                  header,
+                  description,
+                  timeToRead,
+                  time,
+                  likes,
+                  tag,
+                  imageLink,
+                  postId,
+                  className,
+                  ...props
+              }: CardProps) => {
     return (
         <article {...props} className={cn(className, styles.card)}>
-            <Image
-                src={imageLink}
-                alt='media'
-                layout='responsive'
-                width={200} height={300}
-                className={styles.media}
-            />
+            <div className={styles.media}>
+                <Image
+                    src={imageLink}
+                    alt='media'
+                    fill
+                    style={{
+                        objectFit: 'contain',
+                    }}
+                />
+            </div>
             <div className={cn(styles.info)}>
                 <div className={cn(styles.info__tag)}>
                     <span className={cn(styles.text)}>{tag}</span>
